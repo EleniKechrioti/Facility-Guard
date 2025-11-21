412b34a2692e23a5d66a5f3f23f12d3f2a875461
DimitrisBls <dim.br94@gmail.com>
2025-11-21 02:10:32 +0200

    Domain Exception added

---
+++ FacilityGuard/src/main/java/org/aueb/domain/DomainException.java
@@ -0,0 +1,12 @@
+package org.aueb.domain;
+
+
+public class DomainException extends RuntimeException {
+
+    public DomainException() {
+    }
+
+    public DomainException(String message) {
+        super(message);
+    }
+}
